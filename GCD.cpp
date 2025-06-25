6e77ba63120994c44b2d23794b47ed2bdb717c9b
Gajal Garg <gajalgarg09@gmail.com>
2025-06-25 19:05:26 +0530

    Add files via upload

---
+++ GCD.cpp
@@ -0,0 +1,14 @@
+// Given two positive integers a and b, find GCD of a and b.
+
+class Solution {
+  public:
+    int gcd(int a, int b) {
+       
+       while(a > 0 && b>0){
+           if(a > b) a = a % b;
+           else b = b % a;
+       }
+       if(a == 0) return b;
+       return a;
+    }
+};
